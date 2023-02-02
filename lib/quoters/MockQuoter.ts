e191e9cbb20da95664ddf657e7bc04a6d76627de
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-02-02 17:02:48 -0500

    remove autorouter quoter

--- lib/quoters/MockQuoter.ts
+++ lib/quoters/MockQuoter.ts
@@ -1,7 +1,8 @@
 import Logger from 'bunyan';
+import { BigNumber } from 'ethers';
 
-import { QuoteRequest, QuoteResponse } from '../entities';
 import { Quoter, QuoterType } from '.';
+import { QuoteRequest, QuoteResponse } from '../entities';
 
 export const MOCK_FILLER_ADDRESS = '0x0000000000000000000000000000000000000001';
 
@@ -14,7 +15,15 @@ export class MockQuoter implements Quoter {
   }
 
   public async quote(request: QuoteRequest): Promise<QuoteResponse[]> {
-    const amountOut = request.amountIn.mul(this.numerator).div(this.denominator);
+    let amountOut;
+    if (
+      request.tokenIn === '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' &&
+      request.tokenOut === '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+    ) {
+      amountOut = BigNumber.from(2000);
+    } else {
+      amountOut = request.amountIn.mul(this.numerator).div(this.denominator);
+    }
     this.log.info(
       `MockQuoter: request ${request.requestId}: ${request.amountIn.toString()} -> ${amountOut.toString()}`
     );
