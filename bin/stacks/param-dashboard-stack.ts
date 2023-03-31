fbee45245b54d3d1fef0fc1553329fb65e533691
Mark Toda <toda.mark@gmail.com>
2023-03-31 12:29:36 -0700

    fix: stuff

--- bin/stacks/param-dashboard-stack.ts
+++ bin/stacks/param-dashboard-stack.ts
@@ -4,7 +4,7 @@ import { Construct } from 'constructs'
 
 export const NAMESPACE = 'Uniswap'
 
-export type MetricPath = string | { expression?: string, visible?: boolean, id?: string, label?: string };
+export type MetricPath = string | { expression?: string, visible?: boolean, id?: string, label?: string, region?: string };
 
 export type LambdaWidget = {
   type: string
@@ -19,8 +19,14 @@ export type LambdaWidget = {
     metrics: MetricPath[][];
     region: string;
     title: string;
-    stat: string
-  }
+    stat: string,
+    yAxis?: {
+      left: {
+        label: string,
+        showUnits: boolean
+      }
+    }
+  },
 }
 
 const LatencyWidget = (region: string): LambdaWidget => ({
@@ -89,16 +95,24 @@ const ErrorRatesWidget = (region: string): LambdaWidget => ({
   type: "metric",
   properties: {
     metrics: [
-      [ { expression: "100*(m2/m1)", label: "200", id: "e1" } ],
-      [ "Uniswap", "QUOTE_RESPONSE_COUNT", "Service", "GoudaParameterizationAPI", { id: "m1", visible: false } ],
-      [ ".", "QUOTE_200", ".", ".", { id: "m2", visible: false } ]
+      [ { expression: "100*(m2/m4)", label: "200", id: "e1", region } ],
+      [ { expression: "100*(m3/m4)", label: "404", id: "e2", region } ],
+      [ "Uniswap", "QUOTE_200", "Service", "GoudaParameterizationAPI", { id: "m2", visible: false } ],
+      [ ".", "QUOTE_404", ".", ".", { id: "m3", visible: false } ],
+      [ ".", "QUOTE_REQUESTED", ".", ".", { id: "m4", visible: false } ]
     ],
     view: "timeSeries",
-    stacked: false,
+    stacked: true,
     region,
-    stat: "Average",
+    stat: "Sum",
     period: 300,
-    title: "Error Rates"
+    title: "Error Rates",
+    yAxis: {
+      left: {
+        label: "Percent",
+        showUnits: false
+      }
+    }
   }
 })
 
@@ -110,16 +124,24 @@ const RFQFailRatesWidget = (region: string): LambdaWidget => ({
   type: "metric",
   properties: {
     metrics: [
-      [ { expression: "100*(m1/m2)", label: "***REMOVED*** Traders", id: "e1" } ],
+      [ { expression: "100*((m1+m3+m4)/m2)", label: "***REMOVED*** Traders", id: "e1", region } ],
       [ "Uniswap", "RFQ_FAIL_ERROR_https://rfq.***REMOVED***/gouda-rfqs", "Service", "GoudaParameterizationAPI", { id: "m1", visible: false } ],
-      [ ".", "RFQ_REQUESTED_https://rfq.***REMOVED***/gouda-rfqs", ".", ".", { id: "m2", visible: false } ]
+      [ ".", "RFQ_REQUESTED_https://rfq.***REMOVED***/gouda-rfqs", ".", ".", { id: "m2", visible: false } ],
+      [ ".", "RFQ_SUCCESS_https://rfq.***REMOVED***/gouda-rfqs", ".", ".", { id: "m3", visible: false } ],
+      [ ".", "RFQ_FAIL_VALIDATION_https://rfq.***REMOVED***/gouda-rfqs", ".", ".", { id: "m4", visible: false } ]
     ],
     view: "timeSeries",
     stacked: false,
     region,
-    stat: "Average",
+    stat: "Sum",
     period: 300,
-    title: "RFQ Fail Rates"
+    title: "RFQ Fail Rates",
+    yAxis: {
+      left: {
+        label: "Percent",
+        showUnits: false
+      }
+    }
   }
 })
 
