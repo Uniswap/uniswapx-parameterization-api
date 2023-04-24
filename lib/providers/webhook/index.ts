244587fb9f845c98f2af1950e2e50a6140c53a08
Mark Toda <toda.mark@gmail.com>
2023-04-24 13:32:39 -0700

    fix: type overrides

--- lib/providers/webhook/index.ts
+++ lib/providers/webhook/index.ts
@@ -1,10 +1,14 @@
 export * from './mock';
 export * from './s3';
 
+type WebhookOverrides = {
+  timeout: number;
+};
+
 export interface WebhookConfiguration {
   endpoint: string;
   headers?: { [key: string]: string };
-  overrides?: { [key: string]: string };
+  overrides?: WebhookOverrides;
 }
 
 export interface WebhookConfigurationProvider {
