1bd110af95f9ca522e10cb6d9f433fd088c6cd85
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-08-29 00:00:53 +0800

    rename s3 key constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const WEBHOOK_CONFIG_BUCKET = 'rfq-config';
 export const SYNTH_SWITCH_BUCKET = 'synthetic-switch';
-export const INTEGRATION_WEBHOOK_CONFIG_KEY = 'integration.json';
+export const INTEGRATION_S3_KEY = 'integration.json';
 export const PRODUCTION_S3_KEY = 'production.json';
 
 export const DYNAMO_TABLE_NAME = {
