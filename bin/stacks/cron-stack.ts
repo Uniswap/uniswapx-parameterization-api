5516b1a769ee261ee8ecd45cadecf64cfd49bb98
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-09-15 14:10:04 -0400

    bump synth switch cron timeout

--- bin/stacks/cron-stack.ts
+++ bin/stacks/cron-stack.ts
@@ -75,7 +75,7 @@ export class CronStack extends cdk.NestedStack {
       runtime: aws_lambda.Runtime.NODEJS_18_X,
       entry: path.join(__dirname, '../../lib/cron/synth-switch.ts'),
       handler: 'handler',
-      timeout: Duration.seconds(600), // should be more than enough
+      timeout: Duration.minutes(10), // should be more than enough
       memorySize: 1024,
       bundling: {
         minify: true,
@@ -90,7 +90,7 @@ export class CronStack extends cdk.NestedStack {
     });
     new aws_events.Rule(this, `${SERVICE_NAME}SynthSwitchSchedule`, {
       // TODO: fix schedule
-      schedule: aws_events.Schedule.rate(Duration.minutes(10)),
+      schedule: aws_events.Schedule.rate(Duration.minutes(15)),
       targets: [new aws_events_targets.LambdaFunction(this.synthSwitchCronLambda)],
     });
 
