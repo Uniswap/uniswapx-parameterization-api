e64da8dd375094e283c8089f53ca64fa1812ddf1
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-09-19 10:48:59 -0400

    so far

--- lib/quoters/index.ts
+++ lib/quoters/index.ts
@@ -1,4 +1,5 @@
 import { QuoteRequest, QuoteResponse } from '../entities';
+import { CircuitBreakerConfiguration } from '../providers/circuit-breaker';
 
 export enum QuoterType {
   TEST = 'TEST',
@@ -8,6 +9,7 @@ export enum QuoterType {
 
 export interface Quoter {
   quote(request: QuoteRequest): Promise<QuoteResponse[]>;
+  consumeCircuitBreakerConfig(config: CircuitBreakerConfiguration[]): void;
   type(): QuoterType;
 }
 
