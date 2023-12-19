e1477c718cb17a566b54830e8219c20ee06d0b15
Riley Campbell <rileydcampbell@berkeley.edu>
2023-12-19 08:43:27 -0800

    chore: add speedy jest test compiler

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,13 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['bin', 'dist'],
   collectCoverageFrom: ['**/*.ts', '!**/build/**', '!**/node_modules/**', '!**/dist/**', '!**/bin/**'],
+  transform: {
+    // Use swc to speed up ts-jest's sluggish compilation times.
+    // Using this cuts the initial time to compile from 6-12 seconds to
+    // ~1 second consistently.
+    // Inspiration from: https://github.com/kulshekhar/ts-jest/issues/259#issuecomment-1332269911
+    //
+    // https://swc.rs/docs/usage/jest#usage
+    '^.+\\.(t|j)s?$': '@swc/jest',
+  },
 };
