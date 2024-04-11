942684fa23c55e37eaca3ce6498d125aaba9e4cb
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-04-11 12:11:37 -0400

    enable 7872

--- lib/quoters/WebhookQuoter.ts
+++ lib/quoters/WebhookQuoter.ts
@@ -35,7 +35,7 @@ export class WebhookQuoter implements Quoter {
     private webhookProvider: WebhookConfigurationProvider,
     private circuitBreakerProvider: CircuitBreakerConfigurationProvider,
     private complianceProvider: FillerComplianceConfigurationProvider,
-    _allow_list: Set<string> = new Set<string>(['1ed189c4b20479e36acf74e2bc87e03bfdce765ecba6696970caee8299fc005f'])
+    _allow_list: Set<string> = new Set<string>(['7872a1ece724cefa93da6f95fa6b7360a7f3d2c5b21a6b9cfceca362a76843f4'])
   ) {
     this.log = _log.child({ quoter: 'WebhookQuoter' });
     this.ALLOW_LIST = _allow_list;
