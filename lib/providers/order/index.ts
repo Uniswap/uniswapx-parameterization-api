8824dec6c01255a4784c8f9344085f439c29402c
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-04-19 10:38:04 -0400

    remove unused interface

--- lib/providers/order/index.ts
+++ lib/providers/order/index.ts
@@ -1,12 +1,6 @@
 import { Order } from '@uniswap/uniswapx-sdk';
 import { ErrorResponse } from '../../handlers/base';
 
-export interface OrderServiceErrorResponse {
-  statusCode?: number;
-  errorCode?: string;
-  detail?: string;
-}
-
 export interface OrderServiceProvider {
   postOrder(order: Order, signature: string, quoteId?: string): Promise<ErrorResponse | void>;
 }
