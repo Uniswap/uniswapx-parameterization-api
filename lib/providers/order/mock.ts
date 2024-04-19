553ada47c1222cf16bdac64fa7ae9384fdbd7bf5
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-04-19 14:24:51 -0400

    fix mock return type

--- lib/providers/order/mock.ts
+++ lib/providers/order/mock.ts
@@ -1,13 +1,22 @@
 import { Order } from '@uniswap/uniswapx-sdk';
 
-import { OrderServiceProvider } from '.';
+import { OrderServiceProvider, UniswapXServiceResponse } from '.';
+import { ErrorResponse } from '../../handlers/base';
 
 export class MockOrderServiceProvider implements OrderServiceProvider {
   public orders: string[] = [];
 
   constructor() {}
 
-  async postOrder(order: Order, _signature: string, _quoteId?: string): Promise<void> {
+  async postOrder(
+    order: Order,
+    _signature: string,
+    _quoteId?: string
+  ): Promise<ErrorResponse | UniswapXServiceResponse> {
     this.orders.push(order.serialize());
+    return {
+      statusCode: 200,
+      data: 'Order posted',
+    };
   }
 }
