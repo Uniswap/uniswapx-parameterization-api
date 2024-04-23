d6921d50af137e17501407b3dc2b11bdf4be50eb
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-04-23 09:51:17 -0400

    fix dynamo table capacity

--- bin/config.ts
+++ bin/config.ts
@@ -3,6 +3,6 @@ import { BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { TableCapacityConfig } from './stacks/cron-stack';
 
 export const PROD_TABLE_CAPACITY: TableCapacityConfig = {
-  fadeRate: { billingMode: BillingMode.PROVISIONED, readCapacity: 50, writeCapacity: 5 },
+  fadeRate: { billingMode: BillingMode.PROVISIONED, readCapacity: 20, writeCapacity: 100 },
   synthSwitch: { billingMode: BillingMode.PROVISIONED, readCapacity: 2000, writeCapacity: 5 },
 };
