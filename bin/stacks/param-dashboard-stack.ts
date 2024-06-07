92682df6a1074484e5a3532284ae579bd0760ac2
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-06-07 12:52:50 -0400

    fix param dashbaord and add cbv2 widget

--- bin/stacks/param-dashboard-stack.ts
+++ bin/stacks/param-dashboard-stack.ts
@@ -3,7 +3,12 @@ import * as aws_cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 import * as aws_lambda_nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
-import { UniswapXParamServiceMetricDimension } from '../../lib/entities';
+import {
+  CircuitBreakerMetricDimension,
+  Metric,
+  SoftQuoteMetricDimension,
+  UniswapXParamServiceMetricDimension,
+} from '../../lib/entities';
 
 export const NAMESPACE = 'Uniswap';
 
@@ -20,8 +25,8 @@ export type MetricPath =
 
 export type LambdaWidget = {
   type: string;
-  x: number;
-  y: number;
+  x?: number;
+  y?: number;
   width: number;
   height: number;
   properties: {
@@ -42,115 +47,107 @@ export type LambdaWidget = {
   };
 };
 
-const LatencyWidget = (region: string): LambdaWidget => ({
-  height: 11,
-  width: 11,
-  y: 11,
-  x: 0,
-  type: 'metric',
-  properties: {
-    metrics: [
-      [
-        'Uniswap',
-        'QUOTE_LATENCY',
-        'Service',
-        UniswapXParamServiceMetricDimension.Service,
-        { stat: 'p90', label: 'p90' },
-      ],
-      [
-        'Uniswap',
-        'QUOTE_LATENCY',
-        'Service',
-        UniswapXParamServiceMetricDimension.Service,
-        { stat: 'p99', label: 'p99' },
-      ],
-      [
-        'Uniswap',
-        'QUOTE_LATENCY',
-        'Service',
-        UniswapXParamServiceMetricDimension.Service,
-        { stat: 'p50', label: 'p50' },
-      ],
-    ],
-    view: 'timeSeries',
-    stacked: false,
-    region,
-    period: 300,
-    title: 'Latency | 5 minutes',
-  },
-});
+const RFQ_SERVICES = [SoftQuoteMetricDimension, UniswapXParamServiceMetricDimension];
 
-const RFQLatencyWidget = (region: string, rfqProviders: string[]): LambdaWidget => ({
-  height: 11,
-  width: 13,
-  y: 11,
-  x: 11,
-  type: 'metric',
-  properties: {
-    metrics: rfqProviders.map((name) => [
-      'Uniswap',
-      `RFQ_RESPONSE_TIME_${name}`,
-      'Service',
-      UniswapXParamServiceMetricDimension.Service,
-      { label: name },
-    ]),
-    view: 'timeSeries',
-    stacked: false,
-    region,
-    stat: 'p90',
-    period: 300,
-    title: 'RFQ Response Times P90 | 5 minutes',
-  },
-});
-
-const QuotesRequestedWidget = (region: string): LambdaWidget => ({
-  height: 11,
-  width: 24,
-  y: 0,
-  x: 0,
-  type: 'metric',
-  properties: {
-    metrics: [
-      ['Uniswap', 'QUOTE_REQUESTED', 'Service', UniswapXParamServiceMetricDimension.Service],
-      ['.', 'QUOTE_200', '.', '.', { visible: false }],
-    ],
-    view: 'timeSeries',
-    region,
-    stat: 'Sum',
-    period: 300,
-    stacked: false,
-    title: 'Quotes Requested | 5 minutes',
-  },
-});
-
-const ErrorRatesWidget = (region: string): LambdaWidget => ({
-  height: 10,
-  width: 11,
-  y: 22,
-  x: 0,
-  type: 'metric',
-  properties: {
-    metrics: [
-      [{ expression: '100*(m2/m4)', label: '200', id: 'e1', region }],
-      [{ expression: '100*(m3/m4)', label: '404', id: 'e2', region }],
-      ['Uniswap', 'QUOTE_200', 'Service', UniswapXParamServiceMetricDimension.Service, { id: 'm2', visible: false }],
-      ['.', 'QUOTE_404', '.', '.', { id: 'm3', visible: false }],
-      ['.', 'QUOTE_REQUESTED', '.', '.', { id: 'm4', visible: false }],
-    ],
-    view: 'timeSeries',
-    stacked: true,
-    region,
-    stat: 'Sum',
-    period: 300,
-    title: 'Error Rates',
-    yAxis: {
-      left: {
-        label: 'Percent',
-        showUnits: false,
+const LatencyWidget = (region: string): LambdaWidget[] =>
+  RFQ_SERVICES.map((service) => {
+    return {
+      height: 11,
+      width: 11,
+      type: 'metric',
+      properties: {
+        metrics: [
+          ['Uniswap', 'QUOTE_LATENCY', 'Service', service.Service, { stat: 'p90', label: 'p90' }],
+          ['Uniswap', 'QUOTE_LATENCY', 'Service', service.Service, { stat: 'p99', label: 'p99' }],
+          ['Uniswap', 'QUOTE_LATENCY', 'Service', service.Service, { stat: 'p50', label: 'p50' }],
+        ],
+        view: 'timeSeries',
+        stacked: false,
+        region,
+        period: 300,
+        title: 'Quote Latency | 5 minutes',
       },
-    },
-  },
-});
+    };
+  });
+
+const RFQLatencyWidget = (region: string, rfqProviders: string[]): LambdaWidget[] =>
+  RFQ_SERVICES.map((service) => {
+    return {
+      height: 11,
+      width: 13,
+      type: 'metric',
+      properties: {
+        metrics: rfqProviders.map((name) => [
+          'Uniswap',
+          `RFQ_RESPONSE_TIME_${name}`,
+          'Service',
+          service.Service,
+          { label: name },
+        ]),
+        view: 'timeSeries',
+        stacked: false,
+        region,
+        stat: 'p90',
+        period: 300,
+        title: 'RFQ Response Times P90 | 5 minutes',
+      },
+    };
+  });
+
+const QuotesRequestedWidget = (region: string): LambdaWidget[] =>
+  RFQ_SERVICES.map((service) => {
+    return {
+      height: 11,
+      width: 24,
+      y: 0,
+      x: 0,
+      type: 'metric',
+      properties: {
+        metrics: [
+          ['Uniswap', 'QUOTE_REQUESTED', 'Service', service.Service],
+          ['.', 'QUOTE_200', '.', '.', { visible: false }],
+        ],
+        view: 'timeSeries',
+        region,
+        stat: 'Sum',
+        period: 300,
+        stacked: false,
+        title: 'Quotes Requested | 5 minutes',
+      },
+    };
+  });
+
+const ErrorRatesWidget = (region: string): LambdaWidget[] =>
+  RFQ_SERVICES.map((service) => {
+    return {
+      height: 10,
+      width: 11,
+      y: 22,
+      x: 0,
+      type: 'metric',
+      properties: {
+        metrics: [
+          [{ expression: '100*(m2/m4)', label: '200', id: 'e1', region }],
+          [{ expression: '100*(m3/m4)', label: '404', id: 'e2', region }],
+          ['Uniswap', 'QUOTE_200', 'Service', service.Service, { id: 'm2', visible: false }],
+          ['.', 'QUOTE_404', '.', '.', { id: 'm3', visible: false }],
+          ['.', 'QUOTE_REQUESTED', '.', '.', { id: 'm4', visible: false }],
+        ],
+        view: 'timeSeries',
+        stacked: true,
+        region,
+        stat: 'Sum',
+        period: 300,
+        title: 'Error Rates',
+        yAxis: {
+          left: {
+            label: 'Percent',
+            showUnits: false,
+          },
+        },
+      },
+    };
+  });
 
 const FailingRFQLogsWidget = (region: string, logGroup: string): LambdaWidget => {
   return {
@@ -169,63 +166,90 @@ const FailingRFQLogsWidget = (region: string, logGroup: string): LambdaWidget =>
   };
 };
 
-const RFQFailRatesWidget = (region: string, rfqProviders: string[]): LambdaWidget => ({
-  height: 10,
-  width: 13,
-  y: 22,
-  x: 11,
-  type: 'metric',
-  properties: {
-    metrics: rfqProviders.flatMap((name, i) => {
-      const rfqRequested = i * 3;
-      const rfqFailError = i * 3 + 1;
-      const rfqFailValidation = i * 3 + 2;
-      return [
-        [
-          {
-            expression: `100*((m${rfqFailError}+m${rfqFailValidation})/m${rfqRequested})`,
-            label: name,
-            id: `e${i}`,
-            region,
+const RFQFailRatesWidget = (region: string, rfqProviders: string[]): LambdaWidget[] =>
+  RFQ_SERVICES.map((service) => {
+    return {
+      height: 10,
+      width: 13,
+      y: 22,
+      x: 11,
+      type: 'metric',
+      properties: {
+        metrics: rfqProviders.flatMap((name, i) => {
+          const rfqRequested = i * 3;
+          const rfqFailError = i * 3 + 1;
+          const rfqFailValidation = i * 3 + 2;
+          return [
+            [
+              {
+                expression: `100*((m${rfqFailError}+m${rfqFailValidation})/m${rfqRequested})`,
+                label: name,
+                id: `e${i}`,
+                region,
+              },
+            ],
+            [
+              'Uniswap',
+              `RFQ_REQUESTED_${name}`,
+              'Service',
+              service.Service,
+              { id: `m${rfqRequested}`, visible: false },
+            ],
+            [
+              'Uniswap',
+              `RFQ_FAIl_ERROR_${name}`,
+              'Service',
+              service.Service,
+              { id: `m${rfqFailError}`, visible: false },
+            ],
+            [
+              'Uniswap',
+              `RFQ_FAIL_VALIDATION_${name}`,
+              'Service',
+              service.Service,
+              { id: `m${rfqFailValidation}`, visible: false },
+            ],
+          ];
+        }),
+        view: 'timeSeries',
+        stacked: false,
+        region,
+        stat: 'Sum',
+        period: 300,
+        title: 'RFQ Fail Rates',
+        yAxis: {
+          left: {
+            label: 'Percent',
+            showUnits: false,
           },
-        ],
-        [
-          'Uniswap',
-          `RFQ_REQUESTED_${name}`,
-          'Service',
-          UniswapXParamServiceMetricDimension.Service,
-          { id: `m${rfqRequested}`, visible: false },
-        ],
-        [
-          'Uniswap',
-          `RFQ_FAIl_ERROR_${name}`,
-          'Service',
-          UniswapXParamServiceMetricDimension.Service,
-          { id: `m${rfqFailError}`, visible: false },
-        ],
-        [
-          'Uniswap',
-          `RFQ_FAIL_VALIDATION_${name}`,
-          'Service',
-          UniswapXParamServiceMetricDimension.Service,
-          { id: `m${rfqFailValidation}`, visible: false },
-        ],
-      ];
-    }),
-    view: 'timeSeries',
-    stacked: false,
-    region,
-    stat: 'Sum',
-    period: 300,
-    title: 'RFQ Fail Rates',
-    yAxis: {
-      left: {
-        label: 'Percent',
-        showUnits: false,
+        },
       },
+    };
+  });
+
+const CircuitBreakerV2Widget = (region: string): LambdaWidget => {
+  return {
+    height: 11,
+    width: 11,
+    type: 'metric',
+    properties: {
+      metrics: [
+        [
+          'Uniswap',
+          Metric.CIRCUIT_BREAKER_V2_BLOCKED,
+          'Service',
+          CircuitBreakerMetricDimension.Service,
+          { stat: 'Average', label: 'avg' },
+        ],
+      ],
+      view: 'timeSeries',
+      stacked: false,
+      region,
+      period: 600,
+      title: 'Blocked Filler On Average | 10 minutes',
     },
-  },
-});
+  };
+};
 
 export interface DashboardProps extends cdk.NestedStackProps {
   quoteLambda: aws_lambda_nodejs.NodejsFunction;
@@ -251,7 +275,8 @@ export class ParamDashboardStack extends cdk.NestedStack {
           ErrorRatesWidget(region),
           RFQFailRatesWidget(region, RFQ_PROVIDERS),
           FailingRFQLogsWidget(region, props.quoteLambda.logGroup.logGroupArn),
-        ],
+          CircuitBreakerV2Widget(region),
+        ].flat(),
       }),
     });
   }
