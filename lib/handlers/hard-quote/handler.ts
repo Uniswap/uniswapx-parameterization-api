7cb753520363fab9ffa1be2f357e5e2e61a6341c
Cody Born <cody.born@uniswap.org>
2024-07-22 15:02:09 -0400

    Extend Arbitrum auction by 1s (#357)

--- lib/handlers/hard-quote/handler.ts
+++ lib/handlers/hard-quote/handler.ts
@@ -222,7 +222,7 @@ function getDecayEndTime(chainId: number, startTime: number): number {
     case ChainId.MAINNET:
       return startTime + 60; // 5 blocks
     case ChainId.ARBITRUM_ONE:
-      return startTime + 7; // 7 seconds
+      return startTime + 8; // 8 seconds
     default:
       return startTime + 30; // 30 seconds
   }
