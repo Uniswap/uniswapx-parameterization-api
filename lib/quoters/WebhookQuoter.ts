019b1556a55e66f4baa1ec21dd6fa2ae496fa533
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-08-28 13:09:42 -0400

    axios.post.catch

--- lib/quoters/WebhookQuoter.ts
+++ lib/quoters/WebhookQuoter.ts
@@ -314,20 +314,17 @@ export class WebhookQuoter implements Quoter {
       timeout: NOTIFICATION_TIMEOUT_MS,
       ...(!!status.webhook.headers && { headers: status.webhook.headers }),
     };
-    try {
-      axios.post(
+    axios
+      .post(
         status.webhook.endpoint,
         {
           blockUntilTimestamp: status.blockUntil,
         },
         axiosConfig
-      );
-    } catch (e) {
-      this.log.error(
-        { endpoint: status.webhook.endpoint, error: e },
-        `Error notifying block to ${status.webhook.endpoint}`
-      );
-    }
+      )
+      .catch((e) => {
+        this.log.error({ endpoint: status.webhook.endpoint, error: e }, `Axios error notifying block`);
+      });
   }
 }
 
