1d5ba89600ef531016cebdc235a14cac48c2af17
Cody Born <cody.born@uniswap.org>
2024-11-30 17:10:14 +0900

    Tighten 5xx alerts

--- bin/stacks/api-stack.ts
+++ bin/stacks/api-stack.ts
@@ -443,10 +443,10 @@ export class APIStack extends cdk.Stack {
       alarmName: 'UniswapXParameterizationAPI-SEV2-5XX',
       metric: api.metricServerError({
         period: Duration.minutes(5),
-        // For this metric 'avg' represents error rate.
-        statistic: 'avg',
+        // For this metric 'sum' represents error count.
+        statistic: 'sum',
       }),
-      threshold: 0.5,
+      threshold: 100,
       // Beta has much less traffic so is more susceptible to transient errors.
       evaluationPeriods: stage == STAGE.BETA ? 5 : 3,
     });
@@ -458,7 +458,7 @@ export class APIStack extends cdk.Stack {
         // For this metric 'sum' represents error count.
         statistic: 'sum',
       }),
-      threshold: 100,
+      threshold: 20,
       // Beta has much less traffic so is more susceptible to transient errors.
       evaluationPeriods: stage == STAGE.BETA ? 5 : 3,
     });
