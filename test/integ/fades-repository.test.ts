d587d37c6d769fcad33330ccbe72c3be23a95a50
Cody Born <cody.born@uniswap.org>
2025-05-20 17:11:02 +0200

    Add simple redshift tests

---
+++ test/integ/fades-repository.test.ts
@@ -0,0 +1,30 @@
+import { RedshiftDataClient } from '@aws-sdk/client-redshift-data';
+import { FadesRepository, V2FadesRepository } from '../../lib/repositories';
+
+describe('FadesRepository Integration Tests', () => {
+  let fadesRepository: FadesRepository;
+  let v2FadesRepository: V2FadesRepository;
+
+  beforeAll(() => {
+    // Create repositories with real Redshift client
+    const client = new RedshiftDataClient({});
+    const configs = {
+      Database: process.env.REDSHIFT_DATABASE!,
+      ClusterIdentifier: process.env.REDSHIFT_CLUSTER_IDENTIFIER!,
+      SecretArn: process.env.REDSHIFT_SECRET_ARN!,
+    };
+
+    fadesRepository = new FadesRepository(client, configs);
+    v2FadesRepository = new V2FadesRepository(client, configs);
+  });
+
+  describe('createFadesView', () => {
+    it('should successfully create V2 fades view', async () => {
+      await expect(v2FadesRepository.createFadesView()).resolves.not.toThrow();
+    });
+    it('should successfully query V2 fades view', async () => {
+      const result = await v2FadesRepository.getFades();
+      expect(result).toBeDefined();
+    });
+  });
+});
