a5d8e7b8a23bb70e0d28b3bd943b797178f934c7
Cody Born <cody.born@uniswap.org>
2025-05-20 17:16:34 +0200

    Verify env vars exist

--- test/integ/fades-repository.test.ts
+++ test/integ/fades-repository.test.ts
@@ -1,16 +1,29 @@
 import { RedshiftDataClient } from '@aws-sdk/client-redshift-data';
 import { V2FadesRepository } from '../../lib/repositories';
+import { checkDefined } from '../../lib/preconditions/preconditions';
 
 describe('FadesRepository Integration Tests', () => {
   let v2FadesRepository: V2FadesRepository;
 
   beforeAll(() => {
-    // Create repositories with real Redshift client
+
+    const REDSHIFT_DATABASE = checkDefined(
+        process.env.REDSHIFT_DATABASE,
+        'Must set REDSHIFT_DATABASE env variable for integ tests. See README'
+    );
+    const REDSHIFT_CLUSTER_IDENTIFIER = checkDefined(
+        process.env.REDSHIFT_CLUSTER_IDENTIFIER,
+        'Must set REDSHIFT_CLUSTER_IDENTIFIER env variable for integ tests. See README'
+    );
+    const REDSHIFT_SECRET_ARN = checkDefined(
+        process.env.REDSHIFT_SECRET_ARN,
+        'Must set REDSHIFT_SECRET_ARN env variable for integ tests. See README'
+    );
     const client = new RedshiftDataClient({});
     const configs = {
-      Database: process.env.REDSHIFT_DATABASE!,
-      ClusterIdentifier: process.env.REDSHIFT_CLUSTER_IDENTIFIER!,
-      SecretArn: process.env.REDSHIFT_SECRET_ARN!,
+      Database: REDSHIFT_DATABASE,
+      ClusterIdentifier: REDSHIFT_CLUSTER_IDENTIFIER,
+      SecretArn: REDSHIFT_SECRET_ARN,
     };
 
     v2FadesRepository = new V2FadesRepository(client, configs);
