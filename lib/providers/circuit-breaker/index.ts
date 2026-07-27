350c4d71f643a691a1a363e22e5bf7ff8e7ebdc7
Alan Wu <60207036+alanhwu@users.noreply.github.com>
2026-07-27 12:01:10 -0400

    chore: remove dead code (#465)

--- lib/providers/circuit-breaker/index.ts
+++ lib/providers/circuit-breaker/index.ts
@@ -1,11 +1,4 @@
-import { FillerTimestampMap } from '../../repositories';
 import { WebhookConfiguration } from '../webhook';
-
-export interface CircuitBreakerConfiguration {
-  hash: string;
-  fadeRate: number;
-  enabled: boolean;
-}
 
 export interface EndpointStatuses {
   enabled: WebhookConfiguration[];
@@ -16,7 +9,5 @@ export interface EndpointStatuses {
 }
 
 export interface CircuitBreakerConfigurationProvider {
-  allow_list?: Set<string>;
-  getConfigurations(): Promise<CircuitBreakerConfiguration[] | FillerTimestampMap>;
   getEndpointStatuses(endpoints: WebhookConfiguration[]): Promise<EndpointStatuses>;
 }
