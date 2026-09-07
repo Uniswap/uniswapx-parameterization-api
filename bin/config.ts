15ff6291fda0c429dd3f9d11bfb720fc539af4cc
Jerome Ibanes <jerome.ibanes@uniswap.org>
2026-09-07 07:36:50 -0700

    filleraddress read bump

--- bin/config.ts
+++ bin/config.ts
@@ -3,6 +3,6 @@ import { BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { TableCapacityConfig } from './stacks/cron-stack';
 
 export const PROD_TABLE_CAPACITY: TableCapacityConfig = {
-  fillerAddress: { billingMode: BillingMode.PROVISIONED, readCapacity: 70, writeCapacity: 250 },
+  fillerAddress: { billingMode: BillingMode.PROVISIONED, readCapacity: 250, writeCapacity: 250 },
   timestamps: { billingMode: BillingMode.PROVISIONED, readCapacity: 100, writeCapacity: 10 },
 };
